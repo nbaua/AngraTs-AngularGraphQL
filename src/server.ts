706b47f029db20f3bf5c58927906cd8dc7094ef4
Nhilesh <66257640+Nhilesh@users.noreply.github.com>
2020-07-12 17:19:52 +0530

    Initial Commit - Basic Hello world server

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import depthLimit from "graphql-depth-limit";
+import { createServer } from "http";
+import cors from "cors";
+import schema from "./gql/schema";
+
+const app = express();
+const server = new ApolloServer({
+  schema,
+  validationRules: [depthLimit(7)],
+});
+
+app.use("*", cors());
+
+server.applyMiddleware({ app, path: "/graphql" });
+
+const httpServer = createServer(app);
+httpServer.listen({ port: 1000 }, (): void =>
+  console.log(`GraphQL server running on http://localhost:1000/graphql`)
+);
